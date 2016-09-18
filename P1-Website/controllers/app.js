b7385ed4e062704a695be35ee37a8a611879c060
Keerush <keerush@live.com>
2016-09-18 17:34:19 -0400

    Initial website commit for P1, with links to empty group description page and member profile page.

---
+++ P1-Website/controllers/app.js
@@ -0,0 +1,19 @@
+var app = angular.module('mainApp', ['ngRoute']);
+
+app.config(function($routeProvider, $locationProvider) {
+	$routeProvider
+	.when('/', {
+		templateUrl: './views/homepage.html'
+	})
+	.when('/about_us', {
+		templateUrl: './views/about_us.html'
+	})
+	.when('/about_members', {
+		templateUrl: './views/about_members.html'
+	})
+	.otherwise({ 
+		redirectTo: '/' 
+	});
+
+	$locationProvider.html5Mode(true);
+});
