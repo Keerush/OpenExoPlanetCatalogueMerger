38a9a12b7dbe5a459a6b15dbf11a8ab5f44622b3
Keerush <keerush@live.com>
2016-10-16 23:10:18 -0400

    Added tabs to deliverable 3 part 1 page and added task board snapshot.

--- P1-Website/controllers/app.js
+++ P1-Website/controllers/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('mainApp', ['ngRoute']);
+var app = angular.module('mainApp', ['ngRoute', 'ui.bootstrap']);
 
 app.config(function($routeProvider, $locationProvider) {
 	$routeProvider
