8718cc6972f6bcaf89ee9b89d687b8034744fd5d
Keerush <keerush@live.com>
2016-10-24 01:22:32 -0400

    Added api call to get NASA data.

--- APP/Group Website/controllers/demoCtrl.js
+++ APP/Group Website/controllers/demoCtrl.js
@@ -1,3 +1,13 @@
-app.controller('demoCtrl', function($scope) {
-	
+app.controller('demoCtrl', function($scope, $http) {
+	$scope.nasaData = '';
+	$scope.error = '';
+
+	$http({
+		method: 'GET',
+		url: 'http://127.0.0.1:8080/api/nasaData'
+	}).then(function successCallBack(res) {
+		$scope.nasaData = res.data;
+	}, function errorCallBack(res) {
+		$scope.error = res.status + " - " + res.statusText;	
+	});
 });
