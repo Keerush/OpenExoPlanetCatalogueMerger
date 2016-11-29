f6024cf1757e3f8f060d994a108c95b5e6408793
Keerush <keerush@live.com>
2016-11-29 16:39:21 -0500

    Updated editNasaFiles.js to ignore differences given in ignoreData.

--- APP/server/models/editNasaFiles.js
+++ APP/server/models/editNasaFiles.js
@@ -1,4 +1,14 @@
 var config = require('../../config.js');
+var mysql = require('promise-mysql');
+var Q = require('q');
+
+// Intialize pool.
+var pool = mysql.createPool({
+	host: config.mysql.host,
+	user: config.mysql.username,
+	password: config.mysql.password,
+	database: config.mysql.database
+});
 
 /*
 	-Format for input data JSON:
@@ -14,5 +24,33 @@ var config = require('../../config.js');
 		list of {name, tableName} pairs
 */
 module.exports = (data) => {
+	var ignoreData = data.ignoreData.map((item) => [item.name, item.tableName]);
 
+	// Update ignored UnderReview data.
+	var promise = new Promise(function(resolve, reject) {
+		var deleteQuery = 'DELETE FROM UnderReview WHERE (keyName, tableName) IN ?';
+		var promises = [];
+		console.log(ignoreData);
+		var promise = pool.getConnection()
+			.then((conn) => {
+				return conn.query(deleteQuery, [[ignoreData]])
+					.then(() => {
+						pool.releaseConnection(conn);
+					});
+			});
+		promises.push(promise);
+
+		// Wait for all promises to be reviewed.
+		Q.all(promises).then(function(data) {
+			console.log('ending pool');
+			resolve('Done');
+			pool.end();
+		}).catch(function(err) {
+			console.log("error somewhere : ", err);
+			resolve('Done');
+			pool.end();
+		});
+	});
+
+	return promise;
 };
