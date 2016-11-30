7d3c62a92b0ed0258013f856d7a9e4d84937e8e1
Keerush <keerush@live.com>
2016-11-29 22:22:27 -0500

    Started on editing xml files from Nasa differences.

--- APP/server/models/editNasaFiles.js
+++ APP/server/models/editNasaFiles.js
@@ -1,6 +1,8 @@
 var config = require('../../config.js');
 var mysql = require('promise-mysql');
 var Q = require('q');
+var xml2js = require('xml2js');
+var fs = require('fs');
 
 // Intialize pool.
 var pool = mysql.createPool({
@@ -10,10 +12,48 @@ var pool = mysql.createPool({
 	database: config.mysql.database
 });
 
+var parser = new xml2js.Parser();
+var builder = new xml2js.Builder({
+	'headless': true,
+	'renderOpts': {
+		'pretty': true,
+		'indent': '\t',
+		'newline': '\n'
+	}
+});
+
+function editFileAttribute(newVal, fileVals) {
+	// Does not have a error plus and minus value.
+	if (typeof fileVals[0] === 'string') {
+		return [newVal];
+	}
+}
+
 /*
 	-Format for editData: 
 		list of objects that was sent by getNasaStar, getNasaSystem, and getNasaPlanet. (see getUnderReview.js)
 */
 module.exports = (editData) => {
-	
+	var promise = new Promise(function(resolve, reject) {
+		// go through each entry, and read the files.
+		editData.forEach((data) => {
+			// convert to js object.
+			var fileLoc = config.masterRepoLocation + '/systems/' + data.filename;
+			fs.readfile(fileLoc, function(err, data) {
+				parser.parseString(data, function(err, result) {
+					var editObj = {};
+					if (data.tableName === 'System') {
+						editObj = data.system;
+						if (data.declination != null) {
+							result.declination = editFileAttribute(data.declination, result.declination);
+						}
+					}
+				});
+			});
+
+			// for each data properity replace file
+		});
+	});
+
+	return promise;
 };
