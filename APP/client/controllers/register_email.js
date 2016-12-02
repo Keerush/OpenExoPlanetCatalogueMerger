d59098a024fcc1dfc8b9e9c50d631e9e3311ee7f
Simon Wong <s.wong7506@gmail.com>
2016-12-01 23:22:38 -0500

    Started on creating email database

--- APP/client/controllers/register_email.js
+++ APP/client/controllers/register_email.js
@@ -2,6 +2,7 @@ app.controller('register_email', function($scope, $http) {
 
   $scope.loginInfo = {};
   $scope.registerEmail = function() {
+    $scope.loginInfo = {firstname, lastname, email};
     console.log($scope.loginInfo);
 
     $http.post('/api/email', $scope.loginInfo).success(function(data) {
