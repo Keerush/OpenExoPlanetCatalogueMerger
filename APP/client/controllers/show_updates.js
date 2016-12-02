535b24e63b72ee09ce54d8644fb95057c4a802b9
Brandon R <Brandon.Ramoudith@mail.utoronto.ca>
2016-12-02 00:40:54 -0500

    attempted to fix git url again

--- APP/client/controllers/show_updates.js
+++ APP/client/controllers/show_updates.js
@@ -1,5 +1,6 @@
 app.controller('show_updates', function($scope, $http, $window, $location) {
 
+    $scope.gitURL = "https://github.com/login/oauth/authorize?client_id=976efe23b3dbd42727fc&scope=public_repo"
     checkGit = function() {
         var searchObject = $location.search();
         if (searchObject['code']) {
