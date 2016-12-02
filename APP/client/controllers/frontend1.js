f3c76f00761e63ac765427aa705e6dbf543a3aab
Keerush <keerush@live.com>
2016-12-02 03:29:35 -0500

    Revert "Created simple login dialog,"
    This reverts commit ed1fd806b40ab31bd2cffaa042cab63ca13cdb0d.

--- APP/client/controllers/frontend1.js
+++ APP/client/controllers/frontend1.js
@@ -1,15 +1,8 @@
 app.controller('frontend', function($scope, $routeParams, $location) {
-	$scope.signedIn = false;
-	
-	$scope.signIn = function() {
-		$scope.signedIn = true;
-		console.log('htest');
+  console.log($routeParams);
+  if ($routeParams.code) {
+    $location.path('/public/show_updates').search({code: $routeParams.code});
 	}
+  }
+);
 
-	console.log($routeParams);
-	if ($routeParams.code) {
-		$location.path('/public/show_updates').search({
-			code: $routeParams.code
-		});
-	}
-});
