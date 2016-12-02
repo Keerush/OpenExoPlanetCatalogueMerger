ed1fd806b40ab31bd2cffaa042cab63ca13cdb0d
Keerush <keerush@live.com>
2016-12-02 03:24:08 -0500

    Created simple login dialog,

--- APP/client/controllers/frontend1.js
+++ APP/client/controllers/frontend1.js
@@ -1,8 +1,15 @@
 app.controller('frontend', function($scope, $routeParams, $location) {
-  console.log($routeParams);
-  if ($routeParams.code) {
-    $location.path('/public/show_updates').search({code: $routeParams.code});
+	$scope.signedIn = false;
+	
+	$scope.signIn = function() {
+		$scope.signedIn = true;
+		console.log('htest');
 	}
-  }
-);
 
+	console.log($routeParams);
+	if ($routeParams.code) {
+		$location.path('/public/show_updates').search({
+			code: $routeParams.code
+		});
+	}
+});
