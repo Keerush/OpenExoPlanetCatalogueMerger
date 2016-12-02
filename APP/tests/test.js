43139faa0bef7f87af8a5dc01526ffbd11fd68ec
Keerush <keerush@live.com>
2016-12-01 22:33:57 -0500

    Cleaned up tests file.

--- APP/tests/test.js
+++ APP/tests/test.js
@@ -3,50 +3,6 @@ var should = require('should');
 
 var server = supertest.agent('http://localhost:8080');
 
-/*describe("Testing database table creation", function() {
-	it('should have status code 200 after tables have been made and with correct password.', function(done) {
-		server
-			.get('/api/createTables/test')
-			.expect(200)
-			.end(function(err, res) {
-				res.status.should.equal(200);
-				done();
-			});
-	}).timeout(5000);
-
-	it('should have status code 404, for invalid password.', function(done) {
-		server
-			.get('/api/createTables/wrongpass')
-			.expect(404)
-			.end(function(err, res) {
-				res.status.should.equal(404);
-				done();
-			});
-	}).timeout(5000);
-});
-
-describe("Testing database table deletion", function() {
-	it('should have status code 200 after tables have been dropped and with correct password.', function(done) {
-		server
-			.get('/api/dropTables/test')
-			.expect(200)
-			.end(function(err, res) {
-				res.status.should.equal(200);
-				done();
-			});
-	}).timeout(5000);
-
-	it('should have status code 404, for invalid password.', function(done) {
-		server
-			.get('/api/dropTables/wrongpass')
-			.expect(404)
-			.end(function(err, res) {
-				res.status.should.equal(404);
-				done();
-			});
-	}).timeout(5000);
-});*/
-
 describe("Testing get Nasa Star differences", function() {
 	it('should have status code 200 after all differences have been pushed.', function(done) {
 		server
@@ -185,3 +141,49 @@ describe("Testing get Eu Planet differences", function() {
 			});
 	}).timeout(10000);
 });
+
+// Create and drop test will affect production db.
+// Will be commented until test server avaiable. 
+/*describe("Testing database table creation", function() {
+	it('should have status code 200 after tables have been made and with correct password.', function(done) {
+		server
+			.get('/api/createTables/test')
+			.expect(200)
+			.end(function(err, res) {
+				res.status.should.equal(200);
+				done();
+			});
+	}).timeout(5000);
+
+	it('should have status code 404, for invalid password.', function(done) {
+		server
+			.get('/api/createTables/wrongpass')
+			.expect(404)
+			.end(function(err, res) {
+				res.status.should.equal(404);
+				done();
+			});
+	}).timeout(5000);
+});
+
+describe("Testing database table deletion", function() {
+	it('should have status code 200 after tables have been dropped and with correct password.', function(done) {
+		server
+			.get('/api/dropTables/test')
+			.expect(200)
+			.end(function(err, res) {
+				res.status.should.equal(200);
+				done();
+			});
+	}).timeout(5000);
+
+	it('should have status code 404, for invalid password.', function(done) {
+		server
+			.get('/api/dropTables/wrongpass')
+			.expect(404)
+			.end(function(err, res) {
+				res.status.should.equal(404);
+				done();
+			});
+	}).timeout(5000);
+});*/
